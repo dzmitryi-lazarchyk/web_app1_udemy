35f85b4c8d8e453f1458c830eba4185fd89aeff4
dzmitryi-lazarchyk <dzmitryi.lazarchyk1994@gmail.com>
2023-02-18 01:02:28 +0300

    Deploy on fly.io

--- web.py
+++ web.py
@@ -3,16 +3,24 @@ import functions
 
 todos = functions.get_todos()
 
+st.set_page_config(layout='wide')
 
 def add_todo():
-    new_todo = st.session_state["new_todo"] + "\n"
-    todos.append(new_todo)
-    functions.store_todos(todos)
+    if st.session_state["new_todo"]:
+        new_todo = st.session_state["new_todo"] + "\n"
+        if new_todo not in todos:
+            todos.append(new_todo)
+            functions.store_todos(todos)
 
 
 st.title("My Todo App")
 st.subheader("This is my todo app.")
-st.write("This app is to increase your productivity.")
+st.write("This app is to increase your <i>productivity</i>.",
+         unsafe_allow_html=True)
+
+st.text_input(label="label", label_visibility='hidden',
+              placeholder="Enter new todo...",
+              on_change=add_todo, key="new_todo")
 
 for todo in todos:
     checkbox = st.checkbox(todo, key=todo)
@@ -22,8 +30,6 @@ for todo in todos:
         del st.session_state[todo]
         st.experimental_rerun()
 
-st.text_input(label="label", label_visibility='hidden',
-              placeholder="Enter new todo...",
-              on_change=add_todo, key="new_todo")
+
 
 
